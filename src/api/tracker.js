c4daad0b500be9fec85d544fcb84b782d6c03a6e
Ciri Ryan <cirijaye@yahoo.com>
2022-04-04 07:27:50 -0700

    connect tracks with track-server start up ngrok add baseURL

---
+++ src/api/tracker.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'http://2d51-71-212-125-67.ngrok.io'
+});
+
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,9 +7,35 @@ const authReducer = (state, action) => {
     }
 };
 
+const signup = (dispatch) => {
+    return ({ email, password }) => {
+        // make api request to signup with email password
+
+        // modify state say auth
+
+        // if fails err message
+    };
+};
+const signin = (dispatch) => {
+    return ({ email, password }) => {
+        // try to signin
+
+        // update state if success
+
+        // if fails show err message
+    };
+
+};
+
+const signout = (dipatch) => {
+    return () => {
+        // signout
+    };
+};
+
 export const { Provider, Context } = createDataContext(
     authReducer,
-    {},
+    { signup, signin, signout},
     { isSignedIn: false }
 );
 
