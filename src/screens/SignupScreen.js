306a66518f43388d456b9b5f9ab4eac387353362
Ciri Ryan <cirijaye@yahoo.com>
2022-04-04 06:17:49 -0700

    add props and state to inputs signup

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,30 +1,49 @@
-import { StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { StyleSheet, View } from 'react-native'
 import { Text, Input, Button } from 'react-native-elements'
 import Spacer from '../components/Spacer'
 
+
 const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
   return (
-    <>
+    <View style={styles.container}>
     <Spacer>
       <Text h3>Sign Up for Tracker</Text>
     </Spacer>
 
     <Spacer />
 
-    <Input label='Email' />
+    <Input 
+      label='Email' 
+      value={email} 
+      onChangeText={setEmail}
+      autoCapitalize='none'
+      autoCorrect={false}
+    />
 
     <Spacer />
 
-    <Input label='Password' />
+    <Input 
+      label='Password'
+      value={password} 
+      onChangeText={setPassword}
+      autoCapitalize='none'
+      autoCorrect={false}
+      secureTextEntry
+    />
     
     <Spacer>
       <Button
         title='Sign Up'
+        style={styles.button}
       />
     </Spacer>
     
-    </>
+    </View>
   )
 }
 
@@ -37,7 +56,14 @@ SignupScreen.navigationOptions = () => {
 };
 
 const styles = StyleSheet.create({
- 
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 200
+  },
+  button: {
+    marginTop: 10
+  }
 });
 
 export default SignupScreen;
